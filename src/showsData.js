c49882a10ada816d7826c8fbfd0b91d2489a2d56
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-30 18:42:39 +0300

    dummy data added

---
+++ src/showsData.js
@@ -0,0 +1,172 @@
+export const showsData = [
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQAZLAKHzQgfI5HKy-a6MQYPLle8zOIIEDEjvPWM1IY5DiK_6kL0zmloPZzXMt9DPPOe_jhCmmGxRuz-_pOThRplqHE.webp?r=b10",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQCSsRdvgzyPwPR9t9lQyoJd5jQFbjtn0Bn1OgOKBSmZW4P7G06fr5OWJYNLKw2f-CF2yEitBRIlpzjClejM5lyQJno.webp?r=4db",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQlfV9w4tYIpOt8YiifK9YWA4BENL-4TZlEC30XTiTyYv01590UX4Q5dqnk68y2tSBcfCC9cJThJlhRJY7v08qxUwkmwSWfe7GcgkOSWl2f8IFQ3Nd6sQQIbdf1e.jpg?r=119",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQqm4oHwrECgAzdpGxYtPw0Umz9y-qR7Hwpu3QRYWJrCCd50xt_vcDCHcae9lEOru1r119pCc7ZcxZNFXuJ_nPofI1165Mm-BofLcq2Q3pO4BnDSzRA7AyaVajS2.jpg?r=211",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQqusUIdn6v-tuCPCWWqhY9epMUcENT2lgBQu1-D1sQ8Roi6br94RGcQhvg-hgSBRW4jyqTJpzWTbCzc_soBUL_TILI.webp?r=9a8",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQvi1h7C9zxE7Yu4HJPtluLcHffS9wVFUOEOXQZ7LmZpSO8Lj0JgT_0-buhfXgaRWE17q4HG6yq5Tloo--Tlr29FMLc.webp?r=18e",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABR3FSVQvszETh2Wshz52xeR9j4bikUhRQrREZqUJySqJ6wLUHjjTzXx7Lkv8CjbJTzt_lmg5x5_saeuG80L9oyHqy4E.webp?r=42f",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRItzLGWEqRojd0SS5lkLN56bRwYGctgeybCwPWq-zs_UwEXKX_4CsXsMlWupAdBB5D7KDN-8RzuCvqzTf5Bvo_HQmI.webp?r=3ad",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRPnYREuSwXXq5mbVbwSb0ma_OOd6BHJheRorgdoi4d3UJkgvSCAMPStaR6D38haMjjlbKFjBUAMtxVFqsj7d8l67qxB5Tp3AhkxGxclzOUqV5V5WMql2NJwM_kd.jpg?r=826",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRZ8h8xBNICWye-31NJo2qqwcDx_aJUtDFrVwDEXN6NIbpZbjzS3NlNjqTpejIaUwXJSzXFo5S9ddlxO3hR6mgDrgCw.webp?r=544",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRrns-SF_71OxrGhcQI31DBD1hCBZbLsBjYQ1S1OFe5-5FFig-7aeJBOupyXxh_tsRS-1RyiUwkPV0jqL9nkV_5FzW8_TPr5YrQl1b0qaWHCrYRLUEVvXzZrO-wK.jpg?r=42f",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRvg1K9EkBbA1Z1zC9AMGFhNPNrgVtnF_R9Xzb7vpJ9CqUfVa6QhfyqFZeolHilbNc4LiAv2NSKSxkj5VWXeOcjb0xUpz2WCH1v4XEy9pP3reT_wsNWMq7AVQ7r_.jpg?r=3a7",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABS0g9bfAtUU55OBFF5FOqmK7DSLzr7bD-PTf5IkXWKWzh6K-OgwpHE47DXptq5vXRiBADsr5ChwcWSCZGYyEDXXEfG4.webp?r=694",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABS8GreTCunAPPb7obOtYYtGXj06OOPQNPCrP9Vp3Kq2F0_zTkChenJMre3Kc2hNrxgoVtOfAMLGHiI9cdLys9g0x2SA.webp?r=d4e",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSRFOqobell_BtSgAu8b6mPAcu-YXMJc-DdURZiPoBPOrpk8rNppKMe_yIHc9gizqLiyWX4W37bsLVw_5EUx1el2X2c.webp?r=eee",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSbYcuNLE7hMCzCr6OTKW2kpYY1aIQzIZRd9hlYyU-n_S9lewhhX1kwtSJDgsHSPW-QOxAX9uHZmALEXJa4uYX5u2iU.webp?r=a05",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSfasLJ2XNUlEW0BhZMNKqr04tzDy-2LJR0_ClZPxalKd9lZS-WtX_XvPwXF9g3892pEETM0SaGUCeAVziUKGcuQ2-o.webp?r=a2d",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT2ilo3pBVssUjQ_6IEIrYvy9UQaycS0ksV-lVpyCFcq4hV-96epooOFWLutypvQm2O63PPjTsGx6-RKxBP5_91kZOA.webp?r=c9f",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABT3vuP3NrWP_Pe0uxyFxG6hB_upACaE-n2bbDRbRyxX0Pq2M5EJr1zNiFMmlP-C3qi9TDG_y7PYUpYQF6fucCnfuqtLcvWPpy9PBDCxIttHOgVAt0PAWuque_83a.jpg?r=6e9",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTEK0o9aMfgmCT-7NarkfUUvADXFsVfTdf-Hb2GFIwxz-3MGeZr-Z2iJhYDNd5DdQ6YSTXVzlfiTgmmU_DWN-tQ-GVc.webp?r=451",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTF5E7LSZQOfz7OOu5ljIWpRoI0XJqNyAjWhzZXSBn-tBc1b_2pK0osCq0aOpfh2JAmG_SKZGDMCePKl51sjv1ogp4U.webp?r=3c1",
+  },
+  {
+    src:
+      "https://occ-0-784-2774.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTInR9FTujVQjNuih_yMT0pY91hZjdFiNy6GyZZz_hWg5UPx6rqDs4mcFGKuEf3mWPEwL7JJEUU_uToUkcWiAunX46Q.webp?r=c15",
+  },
+];
+
+export const titles = [
+  {
+    title: "Diziler",
+  },
+  {
+    title: "Gündemdekiler",
+  },
+  {
+    title: "Listem",
+  },
+  {
+    title: "Tanıdık Filmler",
+  },
+  {
+    title: "Netflix Orjinal İçerikleri",
+  },
+  {
+    title: "Cengizhan, İzlemeye Devam Et",
+  },
+  {
+    title: "Türkiye'de Bugünün Top 10 Listesi",
+  },
+  {
+    title: "Komediler",
+  },
+  {
+    title: "Gözünüzden Kaçan Gizli Hazineler",
+  },
+  {
+    title: "Amerikan Komedi Dizileri",
+  },
+  {
+    title: "Suç Soruşturması Temalı Heyecanlı Diziler",
+  },
+  {
+    title: "Amerikan Dizileri",
+  },
+  {
+    title: "Suç Filmleri",
+  },
+
+  {
+    title: "Türk Yapımı",
+  },
+  {
+    title: "Yeniden İzle",
+  },
+  {
+    title: "Komediler",
+  },
+  {
+    title: "Bilim Kurgu ve Fantastik",
+  },
+  {
+    title: "Yeni Çıkanlar",
+  },
+  {
+    title: "Netflixte Popüler",
+  },
+  {
+    title: "Suç Temalı Komedi Dizileri",
+  },
+  {
+    title: "Bilim Kurgu Dizisi",
+  },
+  {
+    title: "NX: Süper Kahramanlar, Bilim Kurgu, Fantezi ve Daha Fazlası",
+  },
+  {
+    title: "Cesur Filmler",
+  },
+  {
+    title: "Dramalar",
+  },
+  {
+    title: "Komedi Dizileri",
+  },
+  {
+    title: "Aksiyon ve Macera",
+  },
+];

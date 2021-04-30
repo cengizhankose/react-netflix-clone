9d433323523109b7054ee4e305e76db12db45eef
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-30 18:42:25 +0300

    Top 10 component added for banner

---
+++ src/components/Top10/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.css";
+
+const Top10 = () => {
+  return (
+    <div className="redDiv">
+      <p className="redDiv-top">TOP</p>
+      <p className="redDiv-ten">10</p>
+    </div>
+  );
+};
+
+export default Top10;
